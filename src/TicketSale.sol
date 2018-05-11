d3f25ac52351353c5178b15f20996f5bb22cdffb
Daniel Gal <daniel.gal@sch.bme.hu>
2018-05-11 17:37:00 +0200

    Variables, constructor, buyTickets done

--- src/TicketSale.sol
+++ src/TicketSale.sol
@@ -3,15 +3,28 @@ pragma solidity ^0.4.21;
 contract TicketSale {
 
     // <contract_variables>
-
+    address private owner;
+    mapping (address => uint16) tickets;
+    mapping (uint16 => address) ticketsSold;
+    uint16 ticketCount;
+    uint256 private ticketPrice;
     // </contract_variables>
 
-    function TicketSale(uint16 numTickets, uint256 price) public {
+    constructor(uint16 numTickets, uint256 price) public {
         // TODO
+        ticketCount = numTickets;
+        owner = msg.sender;
+        ticketPrice = price;
     }
 
     function buyTicket(uint16 ticketId) public payable {
         // TODO
+        require(ticketId >= 1 && ticketId <= ticketCount); // valid identifier
+        require(ticketsSold[ticketId] == 0); // not sold yet
+        require(tickets[msg.sender] == 0); // no ticket
+        require(msg.value == ticketPrice); // correct amount of Ether
+        tickets[msg.sender] = ticketId;
+        ticketsSold[ticketId] = msg.sender;
     }
 
     function getTicketOf(address person) public view returns (uint16) {
