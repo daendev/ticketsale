d1bb584b58d15afb0b6f9c366696924069a63c4b
Daniel Gal <daniel.gal@sch.bme.hu>
2018-05-11 17:32:13 +0200

    Contract skeleton

---
+++ src/TicketSale.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.21;
+
+contract TicketSale {
+
+    // <contract_variables>
+
+    // </contract_variables>
+
+    function TicketSale(uint16 numTickets, uint256 price) public {
+        // TODO
+    }
+
+    function buyTicket(uint16 ticketId) public payable {
+        // TODO
+    }
+
+    function getTicketOf(address person) public view returns (uint16) {
+        // TODO
+    }
+
+    function offerSwap(address partner) public {
+        // TODO
+    }
+
+    function acceptSwap(address partner) public {
+        // TODO
+    }
+
+    function withdraw() public {
+        // TODO
+    }
+
+}
